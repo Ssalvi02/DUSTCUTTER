5b0de4d3b4cbff7c0e540adcb9a51eaee79d58d6
sSalvi <jaosalvi@protonmail.com>
2024-03-25 01:35:39 -0300

    ene mi go

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+@onready var sprite = $AnimatedSprite3D
+
+@export var move_speed = 2.0
+@export var attack_range = 2.0
+
+@onready var player : CharacterBody3D = get_tree().get_first_node_in_group("player")
+
+var dead = false
+
+func _physics_process(delta):
+	if sprite.animation == "death" and sprite.frame == 5:
+		sprite.position.y = move_toward(sprite.position.y, 0.445, 0.08)
+	if dead:
+		return
+	
+	if player == null:
+		return
+	
+	var dir = player.global_position - global_position
+	dir.y = 0
+	dir = dir.normalized()
+	print(sprite.frame)
+	
+	velocity = dir * move_speed
+	move_and_slide()
+
+func kill():
+	sprite.play("death")
+	dead = true
+	$CollisionShape3D.disabled = true
