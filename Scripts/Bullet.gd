254e40a961548841cef7a0716f2272488c9e591a
ssalvi02 <jaosalvi@protonmai.com>
2024-03-26 16:51:01 -0300

    Sou muito bom no que eu faço pprt

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
+@onready var pierce = get_parent().pierce
 
-const SPEED = 40
+@onready var SPEED = get_parent().bullet_speed
 
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
@@ -19,11 +20,16 @@ func _process(delta):
 	if ray.is_colliding():
 		if ray.get_collider().has_method("kill"):
 			ray.get_collider().kill()
-		mesh.visible = false
-		ray.enabled = false
-		particle.emitting = true
-		await get_tree().create_timer(1.0).timeout
-		queue_free()
+			
+		if(!pierce):
+			mesh.visible = false
+			ray.enabled = false
+			particle.emitting = true
+			await get_tree().create_timer(1.0).timeout
+			queue_free()
+		else:
+			await get_tree().create_timer(1.0).timeout
+			queue_free()
 
 
 func _on_timer_timeout():
